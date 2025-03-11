18193e6408bc2e6b62c5c26b094d0dc8e86f8cba
Ganesh5124 <pvgkumar2001@gmail.com>
2025-03-11 23:55:55 +0530

    added day-4

---
+++ season-1/Day-4/commands.sh
@@ -0,0 +1,15 @@
+sudo yum update -y
+    2  sudo yum install python3 -y
+    3  python3 --version
+    4  pip3 install flask
+    5  sudo apt update
+    6  sudo apt install python3 python3-pip -y
+    7  python3 install flask
+    8  yum install python3 python3-pip -y
+    9  python3 --version
+   10  pip3 install flask
+   11  python3 -m flask --version
+   12  vim flask-app.py
+   13  python3 flask-app.py 
+   14  ls
+   15  history 
